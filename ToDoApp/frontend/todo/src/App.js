8f8815e236ce468e00468e4acc389ad6c5767f2d
Arshadalee <arshadalimulla9@gmail.com>
2024-05-05 22:50:53 +0530

    commit

---
+++ ToDoApp/frontend/todo/src/App.js
@@ -0,0 +1,14 @@
+import CreateTodo from "./comp/CreateTodo";
+import Todos from "./comp/Todos";
+
+
+function App() {
+  return (
+    <div className="App">
+     <CreateTodo/>
+     <Todos/>
+    </div>
+  );
+}
+
+export default App;
